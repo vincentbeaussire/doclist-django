76076033fb744fa0be68a1104dad377659f9fe5b
Vincent Beaussire <vincentpolska@gmail.com>
2023-11-10 15:46:34 +0100

    add_collection

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from tasks.models import Collection
 
 # Create your views here.
 def index(request):
+    context = {}
+    context["collections"] = Collection.objects.order_by("name")
     return render(request, 'tasks/index.html', context={})
+
+
+def add_collection(request):
+    collection_name = request.POST.get("collection-name") #security
+    Collection.objects.create(name=collection_name)
+    return redirect('home')
