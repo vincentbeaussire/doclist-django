43dde364f6d2a4a3e4e64cc9e8194dbdc91ee45e
Vincent Beaussire <vincentpolska@gmail.com>
2023-11-10 14:57:33 +0100

    models and index

--- doclist/urls.py
+++ doclist/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import tasks.views as views
 
 urlpatterns = [
+    path('', views.index, name="home"),
     path('admin/', admin.site.urls),
 ]
