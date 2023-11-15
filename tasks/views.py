0c57b37f8b953d293186c5896c4157ef4ba46639
Vincent Beaussire <vincentpolska@gmail.com>
2023-11-15 16:50:11 +0100

    Afficher une collection via l'URL

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.html import escape
+from django.utils.text import slugify
 
 from tasks.models import Collection, Task
 
 # Create your views here.
 def index(request):
     context = {}
+    collection_slug = request.GET.get("collection")
     collection = Collection.get_default_collection()
+    if collection_slug:
+        collection = get_object_or_404(Collection, slug=collection_slug)
+
+
     context["collections"] = Collection.objects.order_by("slug")
     context["tasks"] = collection.task_set.order_by("description")
     return render(request, 'tasks/index.html', context=context)
@@ -15,7 +21,7 @@ def index(request):
 
 def add_collection(request):
     collection_name = escape(request.POST.get("collection-name")) #security
-    collection, created = Collection.objects.get_or_create(name=collection_name)
+    collection, created = Collection.objects.get_or_create(name=collection_name, slug=slugify(collection_name))
     if not created:
         return HttpResponse("La collection existe déjà.", status=409)
     return HttpResponse(f'<h2>{collection_name}</h2>')
